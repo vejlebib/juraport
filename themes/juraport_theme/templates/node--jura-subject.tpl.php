a8664c761950c079a426d7d5dd607fac528908b4
Simon Holt <simonholt83@gmail.com>
2015-06-01 15:25:44 +0200

    Initial commit

---
+++ themes/juraport_theme/templates/node--jura-subject.tpl.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a node.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728164
+ */
+?>
+<article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
+    <header>
+      <?php print render($title_prefix); ?>
+      <?php if (!$page && $title): ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+    </header>
+  <?php endif; ?>
+
+  <?php
+    // Hide the links so we can render them later.
+    hide($content['links']);
+    print render($content);
+  ?>
+
+  <?php if ($display_maintained_by): ?>
+    <p class="jura-subject-maintained-by">
+      <?php print $maintained_by; ?>
+      <?php print render($last_updated); ?>
+    </p>
+  <?php endif; ?>
+
+  <?php if ($unpublished): ?>
+    <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
+  <?php endif; ?>
+
+  <?php print render($content['links']); ?>
+
+</article>
