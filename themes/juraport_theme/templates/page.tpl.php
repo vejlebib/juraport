3528ecdcfec84e98ccd5c736cb6bd441f49ab8e2
Simon Holt <simonholt83@gmail.com>
2016-08-15 17:24:58 +0200

    Adjusted size and spacing on logo and site name

--- themes/juraport_theme/templates/page.tpl.php
+++ themes/juraport_theme/templates/page.tpl.php
@@ -13,25 +13,25 @@
 <div id="page">
 
   <header class="header" id="header" role="banner">
+    <div class="header-inner">
+      <?php if ($logo): ?>
+        <div class="header__logo-wrapper" id="logo-wrapper">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+        </div>
+      <?php endif; ?>
 
-    <?php if ($logo): ?>
-      <div class="header__logo-wrapper" id="logo-wrapper">
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div class="header__name-and-slogan" id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 class="header__site-name" id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-        <button class="navigation-bar-toggle">
-          <span><?php t('Toggle menu'); ?></span>
-        </button>
-      </div>
-    <?php endif; ?>
+      <?php if ($site_name || $site_slogan): ?>
+        <div class="header__name-and-slogan" id="name-and-slogan">
+          <?php if ($site_name): ?>
+            <h1 class="header__site-name" id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
+          <?php endif; ?>
+          <button class="navigation-bar-toggle">
+            <span><?php t('Toggle menu'); ?></span>
+          </button>
+        </div>
+      <?php endif; ?>
 
     <?php print render($page['header']); ?>
 
@@ -49,6 +49,7 @@
       </div>
     <?php endif; ?>
 
+    </div>
   </header>
 
   <div id="main">
